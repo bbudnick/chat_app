2366a9015216c30221701a9c8166d01fca314b7c
Brita Budnick <bhh2@pdx.edu>
2021-05-20 11:53:09 -0700

    Added dropdown button to each chatroom line

--- src/frontend/components/LineItem.js
+++ src/frontend/components/LineItem.js
@@ -11,19 +11,20 @@
 
 
 import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { apiJoin } from './Api';
 
 let LineItem = (props) => {
     function handleClick(e) {
         e.preventDefault();
         alert(`You clicked chat id` + `${props.id}`);
-        let currentID = props.id; 
+        let currentID = props.id;
         let testUser = {
             "id": currentID,
             "user": "Henry"
         }
         let result = apiJoin(testUser)
-        if(result >= 300 )
+        if (result >= 300)
             alert(`No can do `)
         else
             alert(`You have joined`)
@@ -33,7 +34,23 @@ let LineItem = (props) => {
             <a href="#" onClick={handleClick}>
                 {props.value}
             </a>
-        </ul>)
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </ul>
+
+
+
+    )
 }
+
 
 export default LineItem;
